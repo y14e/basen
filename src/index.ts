eb483a63640220fd28727a6b48a5c54fe7e3acd3
Yusuke Kamiyamane <y@y14e.com>
2026-09-02 20:15:42 +0900

    v1.0.3

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@
  * @author Yusuke Kamiyamane
  * @license MIT
  * @copyright Copyright (c) Yusuke Kamiyamane
- * @see {@link https://github.com/y14e/basen-ts}
+ * @see {@link https://github.com/y14e/basen}
  */
 
 // -----------------------------------------------------------------------------
